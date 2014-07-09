376b539fb22ae57fe441b9bb0df118c5dd328576
Edwin Biemond <biemond@gmail.com>
2014-07-09 17:27:42 +0200

    update for wildfly

--- puppet/manifests/site.pp
+++ puppet/manifests/site.pp
@@ -107,24 +107,11 @@ class my_wildfly{
     install_source    => 'http://download.jboss.org/wildfly/8.1.0.Final/wildfly-8.1.0.Final.tar.gz',
     install_file      => 'wildfly-8.1.0.Final.tar.gz',
     java_home         => '/opt/jdk-8',
-    group             => 'wildfly',
-    user              => 'wildfly',
     dirname           => '/opt/wildfly',
     mode              => 'standalone',
     config            => 'standalone-full-ha.xml',
-    java_xmx          => '512m',
-    java_xms          => '256m',
-    java_maxpermsize  => '256m',
-    mgmt_http_port    => '9990',
-    mgmt_https_port   => '9993',
-    public_http_port  => '8080',
-    public_https_port => '8443',
-    ajp_port          => '8009',
-    users_mgmt        => { 'wildfly' => { 
-                              username => 'wildfly', 
-                              password => '2c6368f4996288fcc621c5355d3e39b7'}
-                          },
   }
+
   wget::fetch { "download sample.war":
     source      => 'https://tomcat.apache.org/tomcat-7.0-doc/appdev/sample/sample.war',
     destination => '/opt/wildfly/standalone/deployments/sample.war',
